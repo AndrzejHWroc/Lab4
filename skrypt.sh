5638e7af3ede576b3d2160bea48af435a7ea2c2f
Andrzej <mrocznycien269@gamil.com>
2025-06-18 00:19:06 +0200

    Dodaj --logs z parametrem liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -4,8 +4,17 @@ case "$1" in
   --date)
     date
     ;;
+  --logs)
+    count=${2:-100}
+    for i in $(seq 1 $count); do
+      filename="log${i}.txt"
+      echo -e "$filename\n$(basename "$0")\n$(date)" > "$filename"
+    done
+    ;;
+
   --help)
     echo "--date       → wyświetla dzisiejszą datę"
+    echo "--logs [n]   → tworzy n plików logx.txt (domyślnie 100)"
     ;;
   *)
     echo "Nieznana opcja. Użyj --help"
