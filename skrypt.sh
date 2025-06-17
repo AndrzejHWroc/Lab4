d5922962ec2ba3e3149d52f80413ea7e4e07709c
Andrzej <mrocznycien269@gamil.com>
2025-06-18 00:22:19 +0200

    Rozszerz --help o nowe flagi

--- skrypt.sh
+++ skrypt.sh
@@ -12,10 +12,13 @@ case "$1" in
     done
     ;;
 
-  --help)
-    echo "--date       → wyświetla dzisiejszą datę"
-    echo "--logs [n]   → tworzy n plików logx.txt (domyślnie 100)"
-    echo "--help       → pokazuje dostępne opcje"
+  --help|-h)
+    echo "Dostępne opcje:"
+    echo "  --date | -d        → wyświetla dzisiejszą datę"
+    echo "  --logs [n] | -l n  → tworzy n plików logx.txt (domyślnie 100)"
+    echo "  --error [n] | -e n → tworzy n folderów errorx/ z plikami errorx.txt"
+    echo "  --init             → klonuje repo Lab4 i ustawia PATH"
+    echo "  --help | -h        → pokazuje dostępne opcje"
     ;;
   *)
     echo "Nieznana opcja. Użyj --help"
